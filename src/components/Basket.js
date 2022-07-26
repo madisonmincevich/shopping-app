6735e8b1c2ba41c14eb88511f1cd3c79c83a7810
madisonmincevich <maddiebooldridge@gmail.com>
2022-07-25 16:16:27 -0800

    Made checkout/payment modal

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "../style/Basket.css";
-import { Link, Routes, Route } from "react-router-dom";
-import Checkout from "./Checkout";
+import confetti from "canvas-confetti";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faApplePay,
+  faCcAmazonPay,
+  faCcPaypal,
+  faCcVisa,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 
 export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
@@ -9,6 +16,40 @@ export default function Basket(props) {
   const taxPrice = itemsPrice * 0.14;
   const shippingPrice = itemsPrice >= 200 ? 0 : 20;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
+
+  function myConfetti() {
+    var duration = 15 * 500;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+      );
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+      );
+    }, 250);
+  }
+
   return (
     <div id="basket">
       <h1 className="cartTitle mt-5">Your Cart</h1>
@@ -46,15 +87,236 @@ export default function Basket(props) {
               <div className="col-2 fw-bold">Total Price</div>
               <div className="col-1 fw-bold">${totalPrice.toFixed(2)}</div>
             </div>
-            <Link to="/checkout">
-              <button className="col-5 mt-3 mb-5">Checkout</button>
-            </Link>
+            <div className="col-5 mt-3 mb-5">
+              <div
+                className="modal fade"
+                id="exampleModalToggle"
+                aria-hidden="true"
+                aria-labelledby="exampleModalToggleLabel"
+                tabIndex="-1"
+              >
+                <div className="modal-dialog modal-dialog-centered">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalToggleLabel">
+                        Your Cart
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <form>
+                        <p className="fs-5">Contact Information</p>
+                        <div className="row">
+                          <div className="col-sm-6">
+                            <input type="text" placeholder="Email" />
+                          </div>
+                          <div className="col-sm-6">
+                            <input
+                              type="tel"
+                              id="phone"
+                              name="phone"
+                              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                              placeholder="Phone number"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-6">
+                            <input
+                              type="text"
+                              placeholder="First Name"
+                              required
+                            />
+                          </div>
+                          <div className="col-6">
+                            <input
+                              type="text"
+                              placeholder="Last Name"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <p className="fs-5">Shipping Information</p>
+
+                        <div className="shipping-address ms-2 me-2">
+                          <div className="row">
+                            <input
+                              type="text"
+                              placeholder="Street Address"
+                              required
+                            />
+                          </div>
+                          <div className="row">
+                            <input type="text" placeholder="Apt/Unit #" />
+                          </div>
+                          <div className="row">
+                            <input type="text" placeholder="City" required />
+                          </div>
+                          <div className="row">
+                            <input type="text" placeholder="State" required />
+                          </div>
+                          <div className="row">
+                            <input type="text" placeholder="Zipcode" required />
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        className="btn btn-primary"
+                        data-bs-target="#exampleModalToggle2"
+                        data-bs-toggle="modal"
+                      >
+                        Continue to Payment
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="modal fade"
+                id="exampleModalToggle2"
+                aria-hidden="true"
+                aria-labelledby="exampleModalToggleLabel2"
+                tabIndex="-1"
+                data-bs-backdrop="false"
+              >
+                <div className="modal-dialog modal-dialog-centered">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalToggleLabel2">
+                        Payment
+                      </h5>
+
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <form>
+                        <div className="row p-3">
+                          <input
+                            type="text"
+                            placeholder="Name on Card"
+                            className="mb-2"
+                          />
+                          <input
+                            type="text"
+                            inputmode="numeric"
+                            pattern="\d*"
+                            placeholder="Credit Card Number"
+                            className="mb-2"
+                          />
+                          <div className="row mb-2">
+                            <div className="col-sm-6 p-0">
+                              <input
+                                type="text"
+                                inputmode="numeric"
+                                placeholder="Expiration Date (MM / YY)"
+                              />
+                            </div>
+                            <div className="col-sm-6">
+                              <input
+                                type="text"
+                                inputmode="numeric"
+                                placeholder="Security Code"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row fs-2 ms-3">
+                          <div className="col-sm-3">
+                            <FontAwesomeIcon icon={faCcVisa} />
+                          </div>
+                          <div className="col-sm-3">
+                            <FontAwesomeIcon icon={faApplePay} />
+                          </div>
+                          <div className="col-sm-3">
+                            <FontAwesomeIcon icon={faCcPaypal} />
+                          </div>
+                          <div className="col-sm-3">
+                            <FontAwesomeIcon icon={faCcAmazonPay} />
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        className="btn btn-primary"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasTop"
+                        aria-controls="offcanvasTop"
+                        onClick={() => myConfetti()}
+                      >
+                        Finalize Purchase
+                      </button>
+
+                      <div
+                        className="offcanvas offcanvas-top"
+                        tabIndex="-1"
+                        id="offcanvasTop"
+                        aria-labelledby="offcanvasTopLabel"
+                        data-bs-backdrop="false"
+                      >
+                        <div className="offcanvas-header">
+                          <div className="row">
+                            <div className="col-sm-11">
+                              <h5 id="offcanvasTopLabel" className="thankyou">
+                                Thank you for your order!
+                              </h5>
+                            </div>
+                            <div className="col-sm-1">
+                              <span className="dumbell">
+                                <FontAwesomeIcon icon={faDumbbell} />
+                              </span>
+                            </div>
+                          </div>
+                          <button
+                            type="button"
+                            className="btn-close text-reset"
+                            data-bs-dismiss="offcanvas"
+                            aria-label="Close"
+                          ></button>
+                        </div>
+                        <div className="offcanvas-body">
+                          <div className="row fs-3">
+                            <p>
+                              Your order will ship within 7-10 business days!
+                            </p>
+                          </div>
+                          <div className="row">
+                            <small>
+                              This is a mock website no real orders are placed
+                            </small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                href="#exampleModalToggle"
+                role="button"
+              >
+                Checkout
+              </a>
+            </div>
           </div>
         )}
       </div>
-      <Routes>
-        <Route path="/checkout" element={<Checkout />}></Route>
-      </Routes>
     </div>
   );
 }
--- src/components/PhotoSlide.js
+++ src/components/PhotoSlide.js
@@ -6,15 +6,15 @@ export default function PhotoSlide() {
     <div className="p-4">
       <div
         id="carouselExampleCaptions"
-        class="carousel slide"
+        className="carousel slide"
         data-bs-ride="carousel"
       >
-        <div class="carousel-indicators">
+        <div className="carousel-indicators">
           <button
             type="button"
             data-bs-target="#carouselExampleCaptions"
             data-bs-slide-to="0"
-            class="active"
+            className="active"
             aria-current="true"
             aria-label="Slide 1"
           ></button>
@@ -31,15 +31,15 @@ export default function PhotoSlide() {
             aria-label="Slide 3"
           ></button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
             <img
               src="https://images.pexels.com/photos/4754144/pexels-photo-4754144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
 "
-              class="d-block w-100"
+              className="d-block w-100"
               alt="..."
             />
-            <div class="carousel-caption d-none d-md-block">
+            <div className="carousel-caption d-none d-md-block">
               <h5 className="fw-bold">Fuel Your Body</h5>
               <p>
                 Choose from our selection of the finest protein bars and
@@ -47,14 +47,14 @@ export default function PhotoSlide() {
               </p>
             </div>
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img
               src="https://images.pexels.com/photos/136404/pexels-photo-136404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
 "
-              class="d-block w-100"
+              className="d-block w-100"
               alt="..."
             />
-            <div class="carousel-caption d-none d-md-block ">
+            <div className="carousel-caption d-none d-md-block ">
               <h5 className="fw-bold">Push Yourself</h5>
               <p>
                 Featuring premium top of the line workout equipment to push
@@ -62,13 +62,13 @@ export default function PhotoSlide() {
               </p>
             </div>
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img
               src="https://images.pexels.com/photos/3112004/pexels-photo-3112004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              class="d-block w-100"
+              className="d-block w-100"
               alt="..."
             />
-            <div class="carousel-caption d-none d-md-block">
+            <div className="carousel-caption d-none d-md-block">
               <h5>Become Stronger</h5>
               <p>
                 Enhance your strength with great quality weight lifting sets on
@@ -78,22 +78,28 @@ export default function PhotoSlide() {
           </div>
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
     </div>
